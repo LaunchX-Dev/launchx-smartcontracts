ca142beec921ecd5f64424c20fe6d6bebd130e1a
antondevdev <86409690+antondevdev@users.noreply.github.com>
2021-07-06 11:32:52 +0300

    Update Token_interface.sol

--- IDO/Token_interface.sol
+++ IDO/Token_interface.sol
@@ -8,7 +8,11 @@ abstract contract Token_interface {
     function owner() public view virtual returns (address);
     function balanceOf(address who) public view virtual returns (uint256);
     
-    function transfer(address _to, uint256 _value) public virtual returns (bool);
+    // function transfer(address _to, uint256 _value) public virtual returns (bool);
     function allowance(address _owner, address _spender) public virtual returns (uint);
-    function transferFrom(address _from, address _to, uint _value) public virtual returns (bool);
+    // function transferFrom(address _from, address _to, uint _value) public virtual returns (bool);
+    
+    // Tether format
+    function transfer(address _to, uint256 _value) public virtual;
+    function transferFrom(address _from, address _to, uint _value) public virtual;
 }
