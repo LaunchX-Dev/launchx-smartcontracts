a4d53409125e3a2da13858cd837c2c60bfc92b5f
v <vladimirfol@gmail.com>
2021-07-15 17:25:56 +0300

    SyntheticDelegation.sol

--- contracts/SyntheticDelegation.sol
+++ contracts/SyntheticDelegation.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {ERC20PresetMinterPauser} from "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 import {ReentrancyGuard} from '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
@@ -13,14 +12,33 @@ import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 ///   to IDO’s and passive yield, in the form of tokens or cash. Yield is in LX tokens.
 /// @notice When tokens are staked, they immediately get locked and user gets synthetic LXP tokens,
 ///   however, tokens are not given access to yield until next two-week cycle.
-contract SyntheticDelegation is Ownable, ReentrancyGuard {  //todo remove ownable
+contract SyntheticDelegation is Ownable, ReentrancyGuard {  //todo remove ownable?
     using SafeERC20 for IERC20;
-    uint256 constant FIRST_MONDAY = 24 * 3600 * 4;  // 01.01.1970 was a Thursday
+    uint256 constant FIRST_MONDAY = 24 * 3600 * 4;  // 01.01.1970 was a Thursday, so 24 * 3600 * 4 is the first Monday python: datetime.datetime.fromtimestamp(24 * 3600 * 4).weekday()
     uint256 constant WINDOW = 2 * 3600 * 24 * 7;   // 2 weeks
     uint256 constant ACTION_WINDOW = 1 * 3600;  // 1 hour to stake/unstake
 
-    uint256 internal _rewardPerLXP;
-    mapping (address => uint256) internal _userClaimed;
+//    struct WithdrawOrder {
+//        uint256 amount;
+//        uint256 lockedTill;
+//    }
+//    mapping (address => mapping(uint256 => WithdrawOrder)) internal _userWithdrawOrder;
+
+    uint256 public totalCurrentPeriodStakeAmount;
+    uint256 public totalNextPeriodStakeAmount;
+    uint256 public globalCachePeriod;
+    uint256[] public periodTotalReward;
+    uint256[] public periodTotalStaked;
+    struct UserProfile {
+        uint256 currentPeriodStake;
+        uint256 nextPeriodStake;
+        uint256 currentPeriodAvailableUnstake;
+        uint256 nextPeriodAvailableUnstake;
+        uint256 cachePeriod;
+        uint256 currentPeriodClaimed;
+    }
+
+    mapping (address => UserProfile) internal _userProfile;
 
     address public LX;
     address public LXP;
@@ -29,62 +47,108 @@ contract SyntheticDelegation is Ownable, ReentrancyGuard {  //todo remove ownabl
     event Stake(address user, uint256 amount);
     event Unstake(address user, uint256 amount);
     event Claim(address user, uint256 amount);
+    event GlobalCacheUpdated(address indexed caller, uint256 indexed previousPeriod, uint256 indexed currentPeriod);
+    event UserCacheUpdated(address indexed caller, address indexed user, uint256 indexed previousPeriod, uint256 indexed currentPeriod);
+    event UserPeriodPayout(address indexed caller, address indexed user, uint256 indexed period, uint256 payout);
 
-    constructor(address _LX) {
+    constructor(address _LX, address _LXP) {
         LX = _LX;
-        LXP = address(new ERC20PresetMinterPauser("LXP", "LXP"));  //todo discuss symbol
+        LXP = _LXP;
+    }
+
+    function getCurrentPeriodIndex() view public returns(uint256) {
+        return (block.timestamp - FIRST_MONDAY) / WINDOW;
     }
 
     function timeFromPeriodStart() view public returns(uint256) {
         return (block.timestamp - FIRST_MONDAY) % WINDOW;
     }
 
+    function updateGlobalCachePeriod() public {
+        uint256 current = getCurrentPeriodIndex();
+        if (current > globalCachePeriod) {
+            emit GlobalCacheUpdated(msg.sender, globalCachePeriod, current);
+            for (uint256 i = globalCachePeriod+1; i < current; i++) {
+                periodTotalStaked[i] = totalNextPeriodStakeAmount;
+            }
+            globalCachePeriod = current;
+            totalCurrentPeriodStakeAmount = totalNextPeriodStakeAmount;
+        }
+    }
+
+    function updateUserCachePeriod(address user) public {  // todo maybe nonReentrant?
+        uint256 current = getCurrentPeriodIndex();
+        UserProfile storage profile = _userProfile[user];
+        if (current > profile.cachePeriod) {
+            emit UserCacheUpdated(msg.sender, user, profile.cachePeriod, current);
+            uint256 reward;
+            for (uint256 i = profile.cachePeriod; i < current; i++) {
+                uint256 iReward = periodRewardPerLXP[i] * profile.currentPeriodStake - profile.currentPeriodClaimed;
+                profile.currentPeriodClaimed += iReward;
+                if (iReward > 0) {
+                    reward += iReward;
+                    emit UserPeriodPayout(msg.sender, user, i, iReward);
+                }
+                profile.cachePeriod = i+1;
+                profile.currentPeriodStake = profile.nextPeriodStake;
+                profile.currentPeriodAvailableUnstake = profile.nextPeriodAvailableUnstake;
+                profile.currentPeriodClaimed = 0;
+            }
+            if (reward > 0) {
+                IERC20(LX).safeTransfer(user, reward);
+            }
+        }
+    }
+
 
     /// @notice Tokens are added every two weeks.
     ///   Users must wait until beginning of a new two-week period for tokens to stake.
     function stake(uint256 amount) external nonReentrant {
-        require(timeFromPeriodStart() < ACTION_WINDOW, "TOO_LATE");
-        _userClaimed[msg.sender] += amount * _rewardPerLXP;  // make it seems like user already claimed all prev reward
+        updateGlobalCachePeriod();
+        updateUserCachePeriod(msg.sender);
+        UserProfile storage profile = _userProfile[msg.sender];
+        profile.nextPeriodStake += amount;
         emit Stake(msg.sender, amount);
         IERC20(LX).safeTransferFrom(msg.sender, address(this), amount);
-        ERC20PresetMinterPauser(LXP).mint(msg.sender, amount);
+        IERC20(LXP).safeTransfer(msg.sender, amount);
     }
 
     /// @notice The contract needs to keep track of users’ running balance from delegation cycle to
     ///   delegation cycle and users need to be able to withdraw from just this yield balance without
     ///   removing themselves from the delegation.
     ///   They also need to be able to read this balance(UI showing balance on staking page)
-    function claimableAmountOfUser(address user) view public returns(uint256) {
-        uint256 balance = IERC20(LXP).balanceOf(user);
-        return balance * _rewardPerLXP - _userClaimed[user];
+    function getPossibleUnstakeAmountOfUserInNextPeriod(address user) public returns(uint256) {
+        updateGlobalCachePeriod();
+        updateUserCachePeriod(user);
+        return _userProfile[user].nextPeriodStake - _userProfile[user].nextPeriodAvailableUnstake;
     }
 
-    function claim() public {  //todo nonReentrant ??
-        require(timeFromPeriodStart() < ACTION_WINDOW, "TOO_LATE");
-        uint256 amount = claimableAmountOfUser(msg.sender);
-        if (amount > 0) {
-            _userClaimed[msg.sender] += amount;
+    function claimToUnstakeInNextPeriod(uint256 amount) public {  //todo nonReentrant ??
+        require(amount <= IERC20(LXP).balanceOf(msg.sender), "NOT_ENOUGH_LXP");
+        uint256 possibleUnstakeAmount = getPossibleUnstakeAmountOfUserInNextPeriod(msg.sender);
+        if (possibleUnstakeAmount > 0) {
+            _userProfile[msg.sender].nextPeriodAvailableUnstake += amount;
+            _userProfile[msg.sender].nextPeriodStake -= amount;
             emit Claim(msg.sender, amount);
-            IERC20(LX).safeTransfer(msg.sender, amount);
+            IERC20(LXP).safeTransferFrom(msg.sender, address(this), amount);
         }
     }
 
     /// @notice When user decides to withdraw, they do not receive tokens until end of two-week delegation cycle,
     ///   but they still need synthetic tokens in wallet to burn to contract.
     function unstake(uint256 amount) external nonReentrant {
-        require(timeFromPeriodStart() < ACTION_WINDOW, "TOO_LATE");  //todo resolve double require
-        require(amount <= IERC20(LXP).balanceOf(msg.sender), "NOT_ENOUGH_LXP");
-        claim();  //todo discuss
-        _userClaimed[msg.sender] -= amount * _rewardPerLXP;  //todo avoid double calculations
-        emit Stake(msg.sender, amount);
-        IERC20(LX).safeTransferFrom(msg.sender, address(this), amount);
-        ERC20PresetMinterPauser(LXP).burnFrom(msg.sender, amount);
+        updateGlobalCachePeriod();
+        updateUserCachePeriod(user);
+        _userProfile[msg.sender].currentPeriodAvailableUnstake -= amount;
+        _userProfile[msg.sender].nextPeriodAvailableUnstake -= amount;
+        emit Unstake(msg.sender, amount);
+        IERC20(LX).transferFrom(msg.sender, amount);
     }
 
     /// @dev Contract also needs ability to receive yield and claim that yield based on share of contract.
     function shareReward(uint256 amount) external nonReentrant {
+        updateGlobalCachePeriod();
         IERC20(LX).safeTransferFrom(msg.sender, address(this), amount);
-        _rewardPerLXP += amount / IERC20(LXP).totalSupply();
-        emit RewardShared(msg.sender, amount);
+        periodTotalReward[getCurrentPeriodIndex()] += amount;
     }
 }
